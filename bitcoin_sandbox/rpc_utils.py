354216627f1be30e1b705d1932f00ee4578db875
Cris Perez <cristina.perez@esat.kuleuven.be>
2019-01-28 18:05:42 +0100

    Adds deprecation warning in getinfo rpc call

--- bitcoin_sandbox/rpc_utils.py
+++ bitcoin_sandbox/rpc_utils.py
@@ -15,6 +15,7 @@ def rpc_getinfo(client, rpc_server, rpc_user=BTC_RPC_USER, rpc_password=BTC_RPC_
     :return: boolean, whether the connection was successful
     """
     try:
+        print("Warning: getinfo was removed in Bitcoin Core 0.16.0")
         rpc_server = get_ip_by_unknown(client, rpc_server)
         # Test connection by sending a getinfo command
         rpc_connection = AuthServiceProxy("http://%s:%s@%s:%s" % (rpc_user, rpc_password, rpc_server, rpc_port))
