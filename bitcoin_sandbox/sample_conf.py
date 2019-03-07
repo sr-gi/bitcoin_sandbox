04c8c01cb5b90768fe304e8b6e6c35a2cd8d4769
Sergi Delgado <sergi.delgado.s@gmail.com>
2019-03-07 20:04:34 +0000

    Updates conf and adds meshnet graph

--- bitcoin_sandbox/sample_conf.py
+++ bitcoin_sandbox/sample_conf.py
@@ -1,14 +1,12 @@
 # Copy this file with your own configuration and save it as conf.py
 
-DOCK_NETWORK_NAME = 'bitcoinnet'
-DOCK_NETWORK_SUBNET ='172.192.1.0/24'
-DOCK_NETWORK_GW = '172.192.1.254'
+DOCK_NETWORK_NAME = 'regtest_net'
+DOCK_NETWORK_SUBNET = '172.192.1.0/24'
+DOCK_NETWORK_GW = '172.192.1.1'
 DOCK_CONTAINER_NAME_PREFIX = 'btc_n'
-DOCK_IMAGE_NAME = 'bitcoin_sandbox'
-
-BTC_RPC_PORT = 18443
-BTC_RPC_USER = 'uab'
-BTC_RPC_PASSWD = 'uabpassword'
+DOCK_IMAGE_NAME = 'sandbox_ln'
 
 LOG_FILE = 'bitcoin_sandbox.log'
-TEST_GRAPH_FILE_1 = './graphs/basic3.graphml'
+BITCOIN_GRAPH_FILE = './graphs/basic_mesh.graphml'
+LN_GRAPH_FILE = './graphs/basic_ln.graphml'
+
